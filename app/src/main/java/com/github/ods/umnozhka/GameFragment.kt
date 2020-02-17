22e4b31d94fc0bfdc84e6ef646afa593d3bda731
Denis Otkidach <denis.otkidach@gmail.com>
2020-02-17 09:25:58 +0300

    Experiment: show standard keyboard with showSoftInput()

--- app/src/main/java/com/github/ods/umnozhka/GameFragment.kt
+++ app/src/main/java/com/github/ods/umnozhka/GameFragment.kt
@@ -1,10 +1,14 @@
 package com.github.ods.umnozhka
 
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.github.ods.umnozhka.databinding.GameFragmentBinding
@@ -27,5 +31,20 @@ class GameFragment : Fragment() {
         binding.viewModel = viewModel
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Log.i("XXX", "Activity: $activity")
+        activity?.let {
+//            it.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN)
+            val requestFocus = view.requestFocus()
+            val requestFocusFromTouch = view.requestFocusFromTouch()
+            Log.i("XXX", "requestFocus: $requestFocus, requestFocusFromTouch: $requestFocusFromTouch")
+            val inputMethodManager = it.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+            val showSoftInput = inputMethodManager.showSoftInput(view, InputMethodManager.SHOW_FORCED)
+            Log.i("XXX", "showSoftInput: $showSoftInput")
+        }
     }
 }
