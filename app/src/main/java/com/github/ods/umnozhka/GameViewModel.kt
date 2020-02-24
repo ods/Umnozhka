b7e84bba238242dcb55d10a1e7c5117930a4a077
Denis Otkidach <denis.otkidach@gmail.com>
2020-02-24 20:19:11 +0300

    onClick for numpad buttons

--- app/src/main/java/com/github/ods/umnozhka/GameViewModel.kt
+++ app/src/main/java/com/github/ods/umnozhka/GameViewModel.kt
@@ -1,5 +1,6 @@
 package com.github.ods.umnozhka
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -10,5 +11,9 @@ class GameViewModel : ViewModel() {
     private val _exerciseText = MutableLiveData("54 × 32 = ____")
     val exerciseText: LiveData<String>
         get() = _exerciseText
+
+    fun onNumClicked(num: Int) {
+        Log.i("XXX", "num=$num")
+    }
 
 }
